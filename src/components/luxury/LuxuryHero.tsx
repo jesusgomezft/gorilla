9c5b12d66ab9b969ed6e99a3da56053cd1fde53b
Jesus Gomez <jesusgomez.s@hotmail.com>
2026-09-23 23:56:28 +0200

    style: reduce hero image scale to make card smaller

--- src/components/luxury/LuxuryHero.tsx
+++ src/components/luxury/LuxuryHero.tsx
@@ -33,7 +33,7 @@ export const LuxuryHero: React.FC<LuxuryHeroProps> = ({ onNavigate, onOpenTechMo
           alt="Gorilla Grading - Master Artwork" 
           className="w-full h-full object-cover object-[75%_center] lg:object-center transition-transform duration-[1200ms] ease-out will-change-transform"
           style={{
-            transform: `scale(1.06) translate3d(${mousePos.x * -12}px, ${mousePos.y * -12}px, 0) rotateY(${mousePos.x * 2}deg) rotateX(${mousePos.y * -2}deg)`
+            transform: `scale(0.95) translate3d(${mousePos.x * -12}px, ${mousePos.y * -12}px, 0) rotateY(${mousePos.x * 2}deg) rotateX(${mousePos.y * -2}deg)`
           }}
         />
         
